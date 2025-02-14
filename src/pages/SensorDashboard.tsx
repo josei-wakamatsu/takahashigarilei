d02a8b5753efd687be417a9668180eafec0c2ad9
josei-wakamatsu <j.wakamatsu@e-showa.net>
2025-02-15 00:40:53 +0900

    Updated .js with latest changes

--- src/pages/SensorDashboard.tsx
+++ src/pages/SensorDashboard.tsx
@@ -8,7 +8,7 @@ const SensorDashboard = () => {
   const [vibrationData, setVibrationData] = useState([]);
   const [temperature, setTemperature] = useState<(number | null)[]>(Array(2).fill(null));
   const [vibration, setVibration] = useState<(number | null)[]>(Array(4).fill(null));
-  const backendUrl = "https://showarealtime.onrender.com";
+  const backendUrl = "https://unozawa-backend.onrender.com";
   const deviceID = "unozawa";
 
   useEffect(() => {
